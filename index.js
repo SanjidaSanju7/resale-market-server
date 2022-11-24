c31ce41e7559a3e47a6a9aa49b653d98ee297e1b
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-11-24 15:35:31 +0600

    get all categories

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,45 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q5qwa13.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+        const categoriesCollection = client.db('resaleMarket').collection('categories');
+
+
+        // all categories
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoriesCollection.find(query);
+            const category = await cursor.toArray();
+            res.send(category);
+        });
+        // categories by id
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const category = await categoriesCollection.findOne(query);
+            res.send(category)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err))
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Resale market server is running')
